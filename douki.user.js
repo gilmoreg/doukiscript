a36ff366d06dbeb59591c3b1b3e51d205c69b314
Grayson Gilmore <gilmoreg@live.com>
2018-09-30 10:53:18 -0700

    Add post call

--- douki.user.js
+++ douki.user.js
@@ -1,15 +1,33 @@
-// ==UserScript== 
+// ==UserScript==
 // @name Douki
 // @namespace http://gilmoreg.com
-// @description Import Anime and Manga Lists from Anilist 
-// @include https://www.myanimelist.net/* 
+// @description Import Anime and Manga Lists from Anilist
+// @include https://myanimelist.net/*
 // ==/UserScript==
 
+(function() {
+    'use strict';
+    const selector = '.header-menu-dropdown > ul > li:last-child';
+    const html = '<li><a aria-role="button" id="anilistImport">Import from Anilist</a></li>';
 
-// <div class="header-menu-dropdown header-profile-dropdown arrow_box" style="display: block;"><ul><li><a href="https://myanimelist.net/profile/solitethos">Profile</a></li><li class="clearfix"><a href="https://myanimelist.net/myfriends.php">Friends</a></li><li class="clearfix"><a href="/clubs.php?action=invitations" title="You have a club invitation." class="fl-r link-count">(1)</a><a href="https://myanimelist.net/clubs.php?action=myclubs">Clubs</a></li><li><a href="https://myanimelist.net/blog/solitethos">Blog Posts</a></li><li><a href="https://myanimelist.net/myreviews.php">Reviews</a></li><li><a href="https://myanimelist.net/myrecommendations.php">Recommendations</a></li><li><a href="https://myanimelist.net/store/bookshelf"><i class="fa fa-book mr4"></i>Bookshelf</a></li><li><a href="https://myanimelist.net/editprofile.php?go=myoptions"><i class="fa fa-cog mr4"></i>Account Settings</a></li><li><form action="https://myanimelist.net/logout.php" method="post"><a href="javascript:void(0);" onclick="$(this).parent().submit();"><i class="fa fa-sign-out mr4"></i>Logout</a></form></li></ul></div>
-
-const selector = '.header-menu-dropdown > ul > li:last-child';
-const html = '<li><button>Import from Anilist</button></li>';
-
-const dropdown = document.querySelector(selector);
-dropdown.insertAdjacentHTML('afterend', html);
+    const dropdown = document.querySelector(selector);
+    console.log(dropdown);
+    dropdown.insertAdjacentHTML('afterend', html);
+    const importButton = document.querySelector('#anilistImport');
+    importButton.addEventListener('click', function() {
+        console.log('importing from anilist');
+        const csrfToken = document.querySelector('meta[name~="csrf_token"]').getAttribute("content");
+        fetch('/ownlist/anime/edit.json', {
+            method: 'post',
+            body: JSON.stringify({
+                csrf_token: csrfToken,
+                anime_id: 36904,
+                num_watched_episodes: 9,
+                status: 1,
+            })
+        })
+            .then((res) => res.json())
+            .then((res) => console.log(res))
+            .catch(err => console.error(err));
+    });
+})();
