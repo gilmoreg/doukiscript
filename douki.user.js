86d9dba2a9a5d804f7bbb9d5dcb0473434cb56c8
Grayson Gilmore <gilmoreg@live.com>
2018-10-14 21:41:07 -0700

    Complete MVP

--- douki.user.js
+++ douki.user.js
@@ -157,22 +157,48 @@ const Anilist = (() => {
   };
 })();
 
-const getMALList = (username) =>
-  fetch(`https://myanimelist.net/animelist/${username}/load.json`).then(res => res.json());
+const sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms));
 
-const malCall = (type, action, data, csrf_token) =>
-  fetch(`/ownlist/${type}/${action}.json`, {
+const getMALList = async (type, username, list = [], page = 1) => {
+  const offset = (page - 1) * 300;
+  const nextList = await fetch(`https://myanimelist.net/${type}list/${username}/load.json?offset=${offset}`).then(res => res.json());
+  if (nextList && nextList.length) {
+    await sleep(1000);
+    return getMALList(type, username, [...list, ...nextList], page + 1);
+  }
+  return [...list, ...nextList];
+}
+
+const malEdit = (type, data) =>
+  fetch(`/ownlist/${type}/edit.json`, {
     method: 'post',
-    body: JSON.stringify(Object.assign(data, {
-      csrf_token
-    }))
+    body: JSON.stringify(data)
   })
-  .then((res) => res.json());
+  .then((res) => {
+    if (res.status === 200) return res;
+    throw new Error({
+      res,
+      data
+    });
+  });
 
-const editAnime = (data, csrf_token) => malCall('anime', 'edit', data, csrf_token);
-const addAnime = (data, csrf_token) => malCall('anime', 'add', data, csrf_token);
-const editManga = (data, csrf_token) => malCall('manga', 'edit', data, csrf_token);
-const addManga = (data, csrf_token) => malCall('manga', 'add', data, csrf_token);
+const malAdd = (type, data) =>
+  fetch(`/ownlist/${type}/add.json`, {
+    method: 'post',
+    headers: {
+      'Accept': '*/*',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'x-requested-with': 'XMLHttpRequest'
+    },
+    body: JSON.stringify(data)
+  })
+  .then((res) => {
+    if (res.status === 200) return res;
+    throw new Error({
+      res,
+      data
+    });
+  });
 
 const getStatus = (status) => {
   // MAL status: 1/watching, 2/completed, 3/onhold, 4/dropped, 6/plantowatch
@@ -192,47 +218,149 @@ const getStatus = (status) => {
   }
 };
 
-const createMALData = (anilistData) => {
+// TODO find out if this respects internationalization
+const buildDateString = (date) =>
+  date.month === 0 && date.day === 0 && date.year === 0 ? null :
+  `${String(date.month).length < 2 ? '0' : ''}${date.month}-${String(date.day).length < 2 ? '0' : ''}${date.day}-${date.year ? String(date.year).slice(-2) : 0}`;
+
+const createMALData = (anilistData, csrf_token) => {
   const result = {
-    anime_id: anilistData.id,
     status: getStatus(anilistData.status),
+    csrf_token,
     score: anilistData.score || 0,
-    num_watched_episodes: anilistData.progress || 0,
-    num_watched_times: anilistData.rewatched || 0,
-    num_watched_times: anilistData.rewatched || 0
-  };
-  if (anilistData.startedAt) {
-    result.start_date = {
-      year: anilistData.startedAt.year || 0,
-      month: anilistData.startedAt.month || 0,
-      day: anilistData.startedAt.day || 0
-    }
-  }
-  if (anilistData.completedAt) {
-    result.finish_date = {
+    finish_date: {
       year: anilistData.completedAt.year || 0,
       month: anilistData.completedAt.month || 0,
       day: anilistData.completedAt.day || 0
-    }
+    },
+    start_date: {
+      year: anilistData.startedAt.year || 0,
+      month: anilistData.startedAt.month || 0,
+      day: anilistData.startedAt.day || 0
+    },
+  };
+  if (anilistData.type === 'anime') {
+    result.num_watched_episodes = anilistData.progress || 0;
+    result.num_watched_times = anilistData.repeat || 0;
+    // result.rewatch_value = anilistData.repeat || 0;
+  } else {
+    result.num_read_chapters = anilistData.progress || 0;
+    result.num_read_volumes = anilistData.progressVolumes || 0;
+    result.num_read_times = anilistData.repeat || 0;
   }
+  result[`${anilistData.type}_id`] = anilistData.id;
   return result;
 };
 
+const createMALHashMap = (malList, type) => {
+  const hashMap = {};
+  malList.forEach(item => {
+    hashMap[item[`${type}_id`]] = item;
+  });
+  return hashMap;
+}
 
-const anilistSync = async () => {
-  const csrfToken = document.querySelector('meta[name~="csrf_token"]').getAttribute("content");
-  const anilistUser = document.querySelector('#douki-anilist-username').value;
-  const malUsername = document.querySelector('.header-profile-link').innerText;
-  const malList = await jikanGetList(malUsername);
-  const list = await Anilist.getList(anilistUser);
-  console.log(malList);
-  const test = list[700];
-  console.log(test);
-  const result = await editAnime(createMALData(test), csrfToken);
+const shouldUpdate = (mal, al) =>
+  Object.keys(al).some(key => {
+    switch (key) {
+      case 'csrf_token':
+        return false;
+      case 'start_date':
+      case 'finish_date':
+        return buildDateString(al[key]) !== mal[`${key}_string`];
+      case 'num_read_chapters':
+      case 'num_read_volumes':
+      case 'num_watched_episodes':
+        // Anlist and MAL have different volume, episode, and chapter counts for some media;
+        // If the item is marked as completed, ignore differences (Status 2 is COMPLETED)
+        {
+          if (mal.status === 2) return false;
+          return al[key] !== mal[key];
+        }
+      default:
+        {
+          // Treat falsy values as equivalent (!= doesn't do the trick here)
+          if (!mal[key] && !al[key]) return false;
+          return al[key] !== mal[key];
+        }
+    }
+  });
 
-  const log = document.querySelector('#douki-sync-log');
-  log.innerHTML += `<li>${test.title}</li>`;
+const anilistSync = async (type, malUsername, anilistList) => {
+  logMessage(`Fetching MyAnimeList ${type} list...`);
+  const malAnimeList = await getMALList(type, malUsername);
+  logMessage(`Fetched MyAnimeList ${type} list.`);
+  const malHashMap = createMALHashMap(malAnimeList, type);
+  const addList = anilistList.filter(item => !malHashMap[item[`${type}_id`]]);
+  const syncList = anilistList.filter(item => {
+    const malItem = malHashMap[item[`${type}_id`]];
+    // Do not try to sync items in the addList
+    if (!malItem) return false;
+    return shouldUpdate(malItem, item);
+  });
+
+  let addedItems = 0;
+  let updatedItems = 0;
+
+  logMessage(`Adding ${addList.length} ${type} items.`);
+
+  for (let item of addList) {
+    await sleep(500);
+    try {
+      await malAdd(type, item);
+      addedItems++;
+    } catch (e) {
+      console.error(JSON.stringify(e));
+      const itemId = item[`${type}_id`];
+      logMessage(`Error adding ${type} <a href="https://myanimelist.net/${type}/${itemId}>${itemId}</a>. Try adding it manually.`);
+    }
+  }
+
+  logMessage(`Added ${addedItems} ${type} items`);
+  logMessage(`Updating ${syncList.length} ${type} items.`);
+
+  for (let item of syncList) {
+    await sleep(500);
+    try {
+      await malEdit(type, item);
+      updatedItems++;
+    } catch (e) {
+      console.error(JSON.stringify(e));
+      const itemId = item[`${type}_id`];
+      logMessage(`Error updating ${type} ${itemId}`);
+    }
+  }
+  logMessage(`Updated ${updatedItems} ${type} items`);
+  logMessage('Import complete.');
 };
+
+const logMessage = (msg) =>
+  document.querySelector('#douki-sync-log').innerHTML += `<li>${msg}</li>`;
+
+const clearLog = () =>
+  document.querySelector('#douki-sync-log').innerHTML = '';
+
+const sync = async (e) => {
+  e.preventDefault();
+  console.clear();
+  clearLog();
+  logMessage(`Fetching data from Anilist...`);
+  const anilistUser = document.querySelector('#douki-anilist-username').value;
+  const anilistList = await Anilist.getList(anilistUser);
+  if (!anilistList || !anilistList.length) {
+    logMessage(`No data found for user ${anilistUser}.`);
+    return;
+  }
+  logMessage(`Fetched Anilist data.`);
+  const csrfToken = document.querySelector('meta[name~="csrf_token"]').getAttribute("content");
+  const anilistInMalFormat = anilistList.map(item => createMALData(item, csrfToken));
+  const anilistAnimeList = anilistInMalFormat.filter(item => item.anime_id);
+  const anilistMangaList = anilistInMalFormat.filter(item => item.manga_id);
+  const malUsername = document.querySelector('.header-profile-link').innerText;
+
+  await anilistSync('anime', malUsername, anilistAnimeList);
+  await anilistSync('manga', malUsername, anilistMangaList);
+}
 
 const addImportForm = () => {
   const html = `
@@ -243,6 +371,7 @@ const addImportForm = () => {
        <p style="margin: 10px"><button id="douki-import">Import</button></p>
      </form>
      <br />
+     <p>Please be patient. If the import goes any faster MAL will ban you for violating the TOS.</p>
      <ul id="douki-sync-log" style="list-type: none;"></ul>
    </div>
 `;
@@ -250,10 +379,7 @@ const addImportForm = () => {
   const element = document.querySelector('#content');
   element.insertAdjacentHTML('afterend', html);
   const importButton = document.querySelector('#douki-import');
-  importButton.addEventListener('click', function (e) {
-    e.preventDefault();
-    anilistSync();
-  });
+  importButton.addEventListener('click', sync);
   const textBox = document.querySelector('#douki-anilist-username');
   textBox.addEventListener('change', function (e) {
     localStorage.setItem('douki-settings', JSON.stringify(e.target.value));
@@ -286,70 +412,3 @@ const addDropDownItem = () => {
     addImportForm();
   }
 })();
-
-/*
-ADD
-fetch("https://myanimelist.net/ownlist/anime/add.json",
-  {
-    "credentials":"include",
-    "headers":{},
-    "referrer":"https://myanimelist.net/addtolist.php?hidenav=1",
-    "referrerPolicy":"no-referrer-when-downgrade",
-    "body":"
-      {\"anime_id\":34577,
-      \"status\":1,
-      \"score\":0,
-      \"num_watched_episodes\":0,
-      \"storage_value\":0,
-      \"storage_type\":0,
-      \"start_date\":{\"year\":0,\"month\":0,\"day\":0},
-      \"finish_date\":{\"year\":0,\"month\":0,\"day\":0},
-      \"num_watched_times\":0,
-      \"rewatch_value\":0,
-      \"csrf_token\":\"c53ffebe02a2e5f7cc08224528a1c48708bc1167\"
-    }",
-    "method":"POST",
-    "mode":"cors"
-  });
-IMPORT
-
-a
-anime_airing_status: 2
-anime_end_date_string: "04-24-99"
-anime_id: 1
-anime_image_path: "https://myanimelist.cdn-dena.com/r/96x136/images/anime/4/19644.jpg?s=58990770819ae823d1cdf8af63383b96"
-anime_licensors: null
-anime_media_type_string: "TV"
-anime_mpaa_rating_string: "R"
-anime_num_episodes: 26
-anime_season: null
-anime_start_date_string: "04-03-98"
-anime_studios: null
-anime_title: "Cowboy Bebop"
-anime_url: "/anime/1/Cowboy_Bebop"
-days_string: null
-finish_date_string: null
-has_episode_video: true
-has_promotion_video: true
-has_video: true
-is_added_to_list: true
-is_rewatching: 0
-num_watched_episodes: 26
-priority_string: null
-score: 10
-start_date_string: null
-status: 2
-storage_string: ""
-tags: ""
-video_url: "/anime/1/Cowboy_Bebop/video"
-__proto__: Object
-b
-anime_id: 1
-finish_date: {year: 0, month: 0, day: 0}
-num_watched_episodes: 26
-num_watched_times: 0
-score: 10
-start_date: {year: 0, month: 0, day: 0}
-status: 2
-__proto__: Object
-*/
