c98c85378e1dadb94df4fb531bb7e79e2be2e60b
Grayson Gilmore <gilmoreg@live.com>
2018-12-29 13:37:10 -0800

    Bump version

--- douki.user.js
+++ douki.user.js
@@ -3,7 +3,7 @@
 // @namespace http://gilmoreg.com
 // @description Import Anime and Manga Lists from Anilist (see https://anilist.co/forum/thread/2654 for more info)
 // @include https://myanimelist.net/*
-// @version 0.1.8
+// @version 0.1.9
 // ==/UserScript==
 
 // Utility Functions
